932d6d8c86b8574c699f20f2eaa1f313837bb385
Vladpru <vladllen96681@gmail.com>
2025-08-01 18:37:16 +0300

    11

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+
+      </div>
+      <ul>
+
+      </ul>
+      <div>
+        
+      </div>
+    </header>
+  )
+}
+
+export default Header
