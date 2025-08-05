c732f48b2901a00ab8bc2bd105be6ff942b1503b
Vladpru <vladllen96681@gmail.com>
2025-08-05 11:12:33 +0300

    x

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section className='relative'>
+    <section className='relative mb-50'>
       <div className='pt-25 flex flex-col justify-center w-full items-center'>
         <div className="flex flex-col justify-center">
           <h2 className="text-4xl text-bec-white font-semibold relative p-10">
@@ -16,8 +16,11 @@ const About = () => {
               className='w-full absolute top-7.5 left-0'
             />
           </h2>
-          <div className="">
-            
+          <div className="relative">
+            <div className="absolute left-0 top-0 h-full w-1 rounded-l-md" />
+            <div className="p-4 bg-bec border-1 border-bec">
+              
+            </div>
           </div>
         </div>
       </div>
