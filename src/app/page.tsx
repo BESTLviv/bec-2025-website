a8daf7632168a6e8e83d6174f616a44e3ab4a8a8
Vladpru <vladllen96681@gmail.com>
2025-08-10 22:13:17 +0300

    changes for deploy3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,13 @@
 'use client'
-import About from "@/components/About";
-import Categories from "@/components/Categories";
-import Bot from "@/components/Bot";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import { useRef, useEffect } from "react";
-import Footer from "@/components/Footer";
-import { useSearchParams, useRouter } from "next/navigation";
+import { Suspense } from "react";
+
+import HomeComponent from "@/components/HomeComponent";
 
 export default function Home() {
-  const HomeRef = useRef<HTMLHeadingElement>(null);
-  const AboutRef = useRef<HTMLHeadingElement>(null);
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  const scrollToSection = (ref: React.RefObject<HTMLElement | null>) => {
-    ref.current?.scrollIntoView({ behavior: 'smooth' });
-  };
-
-  useEffect(() => {
-    if (searchParams.get("scroll") === "about") {
-      scrollToSection(AboutRef);
-
-      setTimeout(() => {
-        router.replace("/", { scroll: false });
-      }, 500);
-    }
-  }, [searchParams, router]);
-
   return (
-    <div>
-      <Header scrollToSection={scrollToSection} refs={{ HomeRef, AboutRef }} />
-      <Hero ref={HomeRef} />
-      <About ref={AboutRef} />
-      <Categories />
-      <Bot />
-      <Footer />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeComponent />
+    </Suspense>
   );
 }
+
---
+++ src/components/HomeComponent.tsx
@@ -0,0 +1,42 @@
+'use client'
+import About from "@/components/About";
+import Categories from "@/components/Categories";
+import Bot from "@/components/Bot";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import { useRef, useEffect } from "react";
+import Footer from "@/components/Footer";
+import { useSearchParams, useRouter } from "next/navigation";
+
+function HomeComponent() {
+  const HomeRef = useRef<HTMLHeadingElement>(null);
+  const AboutRef = useRef<HTMLHeadingElement>(null);
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const scrollToSection = (ref: React.RefObject<HTMLElement | null>) => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    if (searchParams.get("scroll") === "about") {
+      scrollToSection(AboutRef);
+      setTimeout(() => {
+        router.replace("/", { scroll: false });
+      }, 500);
+    }
+  }, [searchParams, router]);
+
+  return (
+    <>
+      <Header scrollToSection={scrollToSection} refs={{ HomeRef, AboutRef }} />
+      <Hero ref={HomeRef} />
+      <About ref={AboutRef} />
+      <Categories />
+      <Bot />
+      <Footer />
+    </>
+  );
+}
+
+export default HomeComponent;
