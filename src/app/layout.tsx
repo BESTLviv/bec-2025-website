8138cd30de5ccd21d8b1604a32bec73cfd693096
Vladpru <vladllen96681@gmail.com>
2025-09-12 15:43:52 +0300

    changed description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: 'BEC 2025 — BEST Engineering Competition',
   description:
-    'BEST Engineering Competitions 2024. Інженерні змагання повертаються. Спробуй себе в одній з двох запропонованих категорій і покажи за ким майбутнє!',
+    'BEST Engineering Competition 2025. Інженерні змагання повертаються. Спробуй себе в одній з двох запропонованих категорій і покажи за ким майбутнє!',
   keywords: [
     'бек',
     'бест львів',
